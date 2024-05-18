c288048c7ea9b93ea786e2acbcc52eae11637394
lordrobincbz <robincrombez.pro@gmail.com>
2024-05-18 22:04:24 +0200

    added fix csplit sonar 4

--- Analyse/analyse-sonar-scanner/latest/scripts/load-certificates.sh
+++ Analyse/analyse-sonar-scanner/latest/scripts/load-certificates.sh
@@ -10,9 +10,7 @@ download_and_install_certificates() {
     echo -n | openssl s_client -showcerts -verify 20 -connect $WEBSITE_URI 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > $CERT_CHAIN_FILE
     csplit -f "./tmp/cert_" -b "%02d.pem.crt" "$CERT_CHAIN_FILE" '/-BEGIN CERTIFICATE-/' '{*}' && \
     rm ./tmp/cert_00.pem.crt && \
-    mkdir -p /usr/local/share/ca-certificates/${WEBSITE_URI%%:*} && \
-    cp ./tmp/*.pem.crt /usr/local/share/ca-certificates/${WEBSITE_URI%%:*}/ && \
-    sudo update-ca-certificates  && \
+    sudo cat ./tmp/cert_*.pem.crt >> /etc/ssl/certs/ca-certificates.crt && \
     echo "Checking if the certificate is added to the system..."  && \
     openssl verify $CERT_CHAIN_FILE && \
     echo "Certificate downloaded and added to the system for $WEBSITE_URI."
